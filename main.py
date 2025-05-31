a0ba3ccbfa867047d611f3a36b91adcbbb988f5f
Len Huang <lendevelops@gmail.com>
2025-05-31 13:39:45 -0400

    error message

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from discord.discord_filtered_read import get_messages
 
 def main():
-    get_messages()
+    if not get_messages():
+        print("Failed to get messages")
+        return
+
 
 if __name__ == "__main__":
     main()
